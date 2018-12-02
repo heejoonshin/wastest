551f095be89ee1ad3f298b5e46bc65a9b9e62552
신희준 <shinhj88@gmail.com>
2018-12-03 04:04:32 +0900

    test폴더 삭제

--- axiotest/src/services/todolist.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export function GetTodo(id){
-    return axios.get('http://localhost:8080/api/todo/' + id)
-}
-
-export function GetTodolist(params){
-    return axios.get('http://localhost:8080/api/todolist' )
-}
